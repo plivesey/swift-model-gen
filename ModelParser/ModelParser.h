81fed0dd6d8b602b0ab9088cccc759bcf6a56786
Peter Livesey <plivesey@linkedin.com>
2017-01-06 09:24:18 -0800

    Created blank project

---
+++ ModelParser/ModelParser.h
@@ -0,0 +1,19 @@
+//
+//  ModelParser.h
+//  ModelParser
+//
+//  Created by Peter Livesey on 1/6/17.
+//  Copyright © 2017 Peter Livesey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModelParser.
+FOUNDATION_EXPORT double ModelParserVersionNumber;
+
+//! Project version string for ModelParser.
+FOUNDATION_EXPORT const unsigned char ModelParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelParser/PublicHeader.h>
+
+
